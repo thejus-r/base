e96c18f8669708d629230f893a04521fe36f88a4
Thejus Rajendran <thejusr1999@icloud.com>
2025-09-03 00:03:53 +0530

    jump game

--- leetcode/45-jump-game-ii.py
+++ leetcode/45-jump-game-ii.py
@@ -1,4 +1,18 @@
 from typing import List
 def jump(nums: List[int]) -> int:
+    n = len(nums)
+    res = 0
+    l = r = 0
 
-    return 0
+    while(r < n - 1):
+        farthest = 0
+        for i in range(l ,r + 1):
+            farthest = max(farthest, i + nums[i])
+        l = r + 1
+        r = farthest
+        res += 1
+    return res
+
+
+print("Example 1:", jump([2, 3, 1, 1, 4])) # 2
+print("Example 2:", jump([2, 3, 0, 1, 4])) # 2
