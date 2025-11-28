e4ee53185f6151c763ff5a60f5557e0a1f7d6fae
Thejus Rajendran <thejusr1999@icloud.com>
2025-11-28 11:18:28 +0530

    Update 2435-paths-in-matrix-whose-sum-is-divisible-by-k.py

--- leetcode/2435-paths-in-matrix-whose-sum-is-divisible-by-k.py
+++ leetcode/2435-paths-in-matrix-whose-sum-is-divisible-by-k.py
@@ -3,32 +3,37 @@ intuition:
     - actual values doesnt matter
     - we can do a dfs or a dp array to keep the state
     - how to keep state tho? still needs to be found
-    
+
+    - can use a 3D DP array to store the state
+    - trying bottom up approach
 
 '''
 def numberOfPaths(grid: list[list[int]], k: int) -> int:
-    mod = 10 ** 9 + 7
+    ROWS, COLS = len(grid), len(grid[0])
+    MOD = 10 ** 9 + 7
 
-    rows, cols = len(grid), len(grid[0])
+    dp = [[[0] * k for _ in range(COLS + 1)] for _ in range(ROWS + 1)]
 
-    # this is for number of paths
-    count = 0
+    target_reminder = (k - (grid[ROWS - 1][COLS - 1] % k )) % k
 
-    # dp array: initalized all zeros
-    dp = [[0] * cols for _ in range (rows)]
+    dp[ROWS - 1][COLS - 1][target_reminder] = 1
 
-    # pre-process: finding the reminder of all, may be not nessasory
-    for i in range (rows):
-        for j in range(cols):
-            grid[i][j] %= k
+    for r in reversed(range(ROWS)):
+        for c in reversed(range(COLS)):
 
-    # depth first search to find the possible paths
+            # Skipping the initial value
+            if r == ROWS - 1 and c == COLS - 1:
+                continue
 
-    print(grid)
-    print(dp)
+            for remain in range(k):
+                new_remain = (remain + grid[r][c]) % k
+                dp[r][c][remain] = (
+                    dp[r + 1][c][new_remain] % MOD + 
+                    dp[r][c + 1][new_remain] % MOD
+                    ) % MOD
 
-    return 0
+    return dp[0][0][0]
 
-print("Example 1: ", numberOfPaths([[5,2,4],[3,0,5],[0,7,2]], 3))
-# print("Example 2: ", numberOfPaths([[7,3,4,9],[2,3,6,2],[2,3,7,0]], 3))
+# print("Example 1: ", numberOfPaths([[5,2,4],[3,0,5],[0,7,2]], 3))
+print("Example 2: ", numberOfPaths([[7,3,4,9],[2,3,6,2],[2,3,7,0]], 3))
 
