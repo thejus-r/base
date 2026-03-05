39181f4f06c5e0e39063fd7ab506712d341c47ad
thejus-r <thejusr1999@icloud.com>
2026-03-05 11:05:20 +0530

    144. Binary Tree Preorder Traversal

---
+++ cpp/leetcode/144.cpp
@@ -0,0 +1,34 @@
+// 144. Binary Tree Preorder Traversal
+
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right): val(x), left(left), right(right) {}
+};
+
+class Solution {
+  public:
+  void preOrder(TreeNode* node, vector<int> &res) {
+      if (node == nullptr) {
+          return;
+      }
+
+      res.push_back(node->val);
+      preOrder(node->left, res);
+      preOrder(node->right, res);
+  }
+  vector<int> preorderTraversal(TreeNode* root) {
+      vector<int> res;
+      preOrder(root, res);
+
+      return res;
+  }
+};
